8e618664bdf079cb35bfe7849e3d16d65df62286
Ferza17 <feryreza85@gmail.com>
2020-06-23 10:55:46 +0700

    Working with EJS

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const expressHbs = require('express-handlebars');
 
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
