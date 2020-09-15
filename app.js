88f8b27eac24658e573adc7780c4130f79e85195
Ferza17 <feryreza85@gmail.com>
2020-09-15 19:00:32 +0700

    üpdate user Schema

--- app.js
+++ app.js
@@ -14,11 +14,22 @@ const shopRoutes = require("./routes/shop");
 
 const errorController = require("./controllers/error");
 
+const User = require("./models/user");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("5f60ac7662f9bf41402b5268")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {});
+});
 
 //Routes
 app.use("/admin", adminRoutes.routes);
@@ -31,7 +42,21 @@ const port = process.env.PORT || 4000;
 mongoose
   .connect("mongodb://localhost:27017/course")
   .then((result) => {
-    console.log(result);
+    User.findOne()
+      .then((user) => {
+        if (!user) {
+          const user = new User({
+            name: "Fery",
+            email: "fery@email.com",
+            cart: {
+              items: [],
+            },
+          });
+          user.save();
+        }
+      })
+      .catch((err) => {});
+
     http.createServer(app).listen(port);
   })
   .catch((err) => {
