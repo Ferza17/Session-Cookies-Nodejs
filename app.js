0d6d0ab8f2f25e1fd20086e0e395524cc4489819
Ferza17 <feryreza85@gmail.com>
2020-10-07 14:47:30 +0700

    update csrf token

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
 /**
@@ -28,46 +30,29 @@ const storeSession = new MongoDBStore({
   uri: MONGODB_URL,
   collection: "sessions",
 });
+const csurfProtection = csrf({ cookie: true });
+// routes Variable
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+// Model Variable
+const User = require("./models/user");
+// Controller Variable
+const errorController = require("./controllers/error");
+
 /**
  *  =============== Global Variabel =============
  */
 
 /**
- *  =============== Routes =============
+ * ========== Initialize all stuf ========
  */
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
-
-/**
- *  =============== END Routes =============
- */
-
-/**
- *  =============== Models  App =============
- */
-const User = require("./models/user");
-/**
- *  =============== End Models  App =============
- */
-
-/**
- *  =============== Controller  App =============
- */
-const errorController = require("./controllers/error");
-/**
- *  =============== End Controller  App =============
- */
-
-/**
- *  =============== Express  App =============
- */
+// Initialize Session Express
 app.set("view engine", "ejs");
 app.set("views", "views");
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-// Initialize Session Express
+app.use(cookieParser());
 app.use(
   session({
     secret: "my secret",
@@ -76,6 +61,28 @@ app.use(
     store: storeSession,
   })
 );
+app.use(csurfProtection);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log("err :>> ", err);
+    });
+});
+
+app.use((req, res, next) => {
+  app.locals.isAuthenticated = req.session.isLoggedIn;
+  app.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 //Routes
 app.use("/admin", adminRoutes.routes);
@@ -83,6 +90,14 @@ app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
+
+/**
+ * ========== End Initialize all stuf ========
+ */
+
+/**
+ *  =============== Express  App =============
+ */
 
 /**
  *  =============== End Express  App =============
